01b47e5ca7a007ac347626f83f1b6402299f85b6
eooomji <dcs0909@naver.com>
2021-03-02 13:56:17 +0900

    1

---
+++ step01_basic/helloPython.py
@@ -0,0 +1,6 @@
+# 주석 ctrl+/
+# 실행 ctrl+shift+F10
+
+print("hello python")   # print 함수 안에 오는 값은 문자열, 따옴표로 표시될 수 있는 값
+print('hello python')   # 문자열 표현 시 홑따옴표, 겹따옴표는 동일한 의미
+print(123)              # 숫자만 출력할 때는 따옴표가 없어도 됨

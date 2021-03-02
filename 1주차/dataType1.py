be023b498d5d1699a75e2bade044cadb3661c80d
eooomji <70639677+eooomji@users.noreply.github.com>
2021-03-02 14:28:49 +0900

    자료형

---
+++ 1주차/dataType1.py
@@ -0,0 +1,17 @@
+name = '뽀로로'    # 문자열 자료형 => 객체 타입
+age = 23          # 정수 자료형
+gender = '남'     # 문자(열) 자료형
+weight = 63.4     # 실수 자료형
+tall = 175.5      # 실수 자료형
+complex_num = 3+4.07j   # 복소수 자료형 (원래는 i로 쓰는데 파이썬에서는 j를 씀)
+
+
+# 한줄 복사 ctrl + d
+#한줄 삭제 ctrl + x
+print('이름:', name,
+      '\n나이:', age,
+      '\n성별:', gender,
+      '\n체중:', weight,
+      '\n신장:', tall,
+      '\n복소수 값:', complex_num,
+      )

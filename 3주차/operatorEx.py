46d600145f7c7eb62c47b335f52baa4aae68358e
eooomji <70639677+eooomji@users.noreply.github.com>
2021-03-16 10:23:25 +0900

    대입 연산, 논리 연산, 관계 연산

---
+++ 3주차/operatorEx.py
@@ -0,0 +1,38 @@
+# 연산자
+
+# 사칙연산 --> 생략
+
+# 대입연산
+a = 3+4
+print(a)
+
+p = 3
+print(p)
+p += 2
+print(p)
+p *= 3
+print(p)
+p -= 2
+print(p)
+p %= 3
+print(p)
+print()
+
+# 논리 연산, 관계 연산
+score = 57
+
+print(score == 57)      # score과 57이 같은가?
+print(score >= 57)      # score가 47보다 크거나 같은가?
+print(score != 57)      # score과 57이 같지 않은가?
+
+string = 'korea'
+print(string == 'korea' or string == 'Korea')
+
+score = 57
+print(score >= 70 and score < 90)   # False
+print(70 <= score < 90)   # False
+print()
+
+print(5%3)
+print(5/3)
+print(5//3)

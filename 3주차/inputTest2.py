05713410efad9532818e7de9ac49ca4f11d6e32a
eooomji <70639677+eooomji@users.noreply.github.com>
2021-03-16 10:22:57 +0900

    값을 입력 받고, 더한 값을 출력하는 test

---
+++ 3주차/inputTest2.py
@@ -0,0 +1,9 @@
+A = int(input("A의 값을 입력하세요 : "))
+B = int(input("B의 값을 입력하세요 : "))
+
+print(A,'+', B, '=', A+B)
+
+C = float(input("A의 값을 입력하세요 : "))
+D = float(input("B의 값을 입력하세요 : "))
+
+print(C,'+', D, '=', C+D)
